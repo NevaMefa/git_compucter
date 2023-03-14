1bda9642cb55380c4d3aaf2fcc263c15db5fbf7f
NevaMefa <k0rbanan@yandex.ru>
2023-03-14 17:34:20 +0300

    initial commit

--- src/javaClass/isClass.java
+++ src/javaClass/isClass.java
@@ -7,12 +7,13 @@ public class isClass {
         person1.age = 50;
         //System.out.println("Меня зовут " + person1.name + "," + "мне " + person1.age);
         // вызовем метод speak
-        person1.sayHello();
+        int year1 = person1.calculateYearsTiRetirement();
+        System.out.println("Первому человеку до пенсии: " +year1+ "лет");
         Person person2 = new Person();
         person2.name = "BoBa";
         person2.age = 20;
-        person2.speak();
-        person2.sayHello();
+        int year2 = person2.calculateYearsTiRetirement();
+        System.out.println("Второму человеку до пенсии: " +year2+ "лет");
         //System.out.println("Меня зовут " + person2.name + "," + "мне " + person2.age);
 
     }
@@ -25,6 +26,19 @@ class Person {
     // РАЗБЕРЕМ ПОЛЯ:
     String name;
     int age;
+
+//тип возвращаемого значени метода, должен быть у каждого метода int, void,string
+
+    int calculateYearsTiRetirement(){
+        int years = 65 - age;
+        return years;
+        //после return нет смысла что-то еще писать,  метод закрываетс
+        // возращаетс только конкретно указанный тип, другой нельз
+    }
+
+
+
+
 
     //Действие по выводу метода с представлением
     void speak() {
