c40557b42129c174b5f3237912c3fe8dd0222026
NevaMefa <natafuuuck@gmail.com>
2023-03-20 19:21:52 +0300

    initial commit

--- src/lesson1516/task4.java
+++ src/lesson1516/task4.java
@@ -10,13 +10,26 @@
 package lesson1516;
 
 public class task4 {
+    // [modifiers] например public static модификаторы функции
+    // возвращающие значения void - ничего не возвращает, int, double, string  - возвращают.
+    //void - ничего не возвращает ЭТО ИСКЛЮЧЕНИЕ
+    // [modifiers] return_value nameVerb([param])
     public static void main(String[] args) {
-        int year = 2000;
-        //число делим на 4 и берем остаток от деления, в данном примере "0"
-        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
-            System.out.println(year + " високосный");
-        } else {
-            System.out.println(year + " не високосный");
-        }
+        int year = 1900;
+        System.out.println("високосный: " + isLeapYear(year));
     }
+        //число делим на 4 и берем остаток от деления, в данном примере "0"
+//        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+//            System.out.println(year + " високосный");
+//        } else {
+//            System.out.println(year + " не високосный");
+//        }
+    // пример 2
+    public static boolean isLeapYear (int year) {
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+//            return true;
+//        } else {
+//            return false;
+//        }
+  }
 }
